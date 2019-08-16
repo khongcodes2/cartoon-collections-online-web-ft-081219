d0a72a11980dff916c112592092b46c4602eef1c
K Hong <albtrossfacefall@gmail.com>
2019-08-16 18:42:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map do |element|
-    element.capitalize.concat("!")
-  end
+  array.map {|element| element.capitalize.concat("!")}
 end
 
 def long_planeteer_calls# code an argument here
